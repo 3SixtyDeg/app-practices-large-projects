0c4504c6d9526a855e2438110921c7caaad5f67f
magranadosb <antonio.granados_dx@outlook.com>
2021-05-22 15:32:40 -0500

    Se agrega guard para validar la navegación entre auth y home.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { AuthGuard } from './auth/guards/auth.guard';
 
 
 
 const routes: Routes = [
+  {
+    path: '', 
+    redirectTo: 'home/dashboard', 
+    pathMatch: 'full'
+  },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
   },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-  },
-  {
-    path: '404',
-    component: ErrorPageComponent
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+    canLoad: [ AuthGuard ],
+    canActivate: [AuthGuard]
   },
   {
     path: '**',
-    redirectTo: '404'
+    component: ErrorPageComponent
   }
 ]
 
