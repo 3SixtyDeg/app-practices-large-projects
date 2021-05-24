6960800fc94871e302697f25a8c0ef853f6cd684
magranadosb <antonio.granados_dx@outlook.com>
2021-05-24 11:53:39 -0500

    Se realiza corrección en el módulo de ventas.

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VentasRoutingModule } from './ventas-routing.module';
-import { VentasDetalleComponent } from './pages/ventas-detalle/ventas-detalle.component';
-import { VentasListaComponent } from './pages/ventas-lista/ventas-lista.component';
 
 
 
 @NgModule({
-  declarations: [
-    VentasDetalleComponent,
-    VentasListaComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     VentasRoutingModule
